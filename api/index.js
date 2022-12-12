13c6673ee180908849867d3cec7756545edef2f2
Usama Baig <usamabaig776@live.com>
2022-12-12 18:50:55 +0500

    finalized API

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-
 const dotenv = require("dotenv");
 dotenv.config();
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -16,6 +22,25 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running");
